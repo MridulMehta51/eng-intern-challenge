c861f23d8bc0ddd44d0367e455c61936274764bc
MridulMehta51 <133112783+MridulMehta51@users.noreply.github.com>
2024-08-31 11:36:15 -0400

    Basic Setup and English to Braille Conversion.

--- python/translator.py
+++ python/translator.py
@@ -1,12 +1,46 @@
+import sys
 
-# Mapping dictionaries for Braille to English and English to Braille conversions
-braille_to_text = {
-    "O.....": "a", "O.O...": "b", "OO....": "c", "OO.O..": "d", "O..O..": "e",
-    "OOO...": "f", "OOOO..": "g", "O.OO..": "h", ".OO...": "i", ".OOO..": "j",
-    "O...O.": "k", "O.O.O.": "l", "OO..O.": "m", "OO.OO.": "n", "O..OO.": "o",
-    "OOO.O.": "p", "OOOOO.": "q", "O.OOO.": "r", ".OO.O.": "s", ".OOOO.": "t",
-    "O...OO": "u", "O.O.OO": "v", ".OOO.O": "w", "OO..OO": "x", "OO.OOO": "y",
-    "O..OOO": "z", ".....O": "capital", ".....O.O.....O.OOOO.O.O.....": " ",
-    ".O.OO.": "1", "..O.OO": "2", ".OO.O.": "3", ".OOOO.": "4", "O..O.O": "5",
-    "OOO.O.": "6", "OOOOO.": "7", "O.OOO.": "8", ".OO.O.": "9", ".OOOO.": "0"
+braille_chars = {
+    'a': 'O.....', 'b': 'O.O...', 'c': 'OO....', 'd': 'OO.O..', 'e': 'O..O..',
+    'f': 'OOO...', 'g': 'OOOO..', 'h': 'O.OO..', 'i': '.OO...', 'j': '.OOO..',
+    'k': 'O...O.', 'l': 'O.O.O.', 'm': 'OO..O.', 'n': 'OO.OO.', 'o': 'O..OO.',
+    'p': 'OOO.O.', 'q': 'OOOOO.', 'r': 'O.OOO.', 's': '.OO.O.', 't': '.OOOO.',
+    'u': 'O...OO', 'v': 'O.O.OO', 'w': '.OOO.O', 'x': 'OO..OO', 'y': 'OO.OOO',
+    'z': 'O..OOO', ' ': '......'
 }
+
+braille_unique = {'capital': '.....O', 'number': '.O.OOO'}
+
+braille_numbers = {
+    '1': 'O.....', '2': 'O.O...', '3': 'OO....', '4': 'OO.O..', '5': 'O..O..',
+    '6': 'OOO...', '7': 'OOOO..', '8': 'O.OO..', '9': '.OO...', '0': '.OOO..',
+}
+
+def english_to_braille(string):
+    result = ''
+    is_number = False
+
+    for char in string:
+        if char.isdigit():
+            if not is_number:
+                is_number = True
+                result += braille_unique['number']
+            result += braille_numbers[char]
+        elif char == ' ':
+            is_number = False
+            result += braille_chars[' ']
+        elif char.isalpha():
+            if char == char.upper():
+                result += braille_unique['capital']
+            result += braille_chars[char.lower()]
+        else:
+            result += '......'  # Fallback for unmapped characters
+
+    return result
+
+def main():
+    message = ' '.join(sys.argv[1:])
+    print(english_to_braille(message))
+
+if __name__ == "__main__":
+    main()
